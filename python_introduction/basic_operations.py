3b6a5db5803d0037475a72fc2a8d8c48a189b0f0
mallah9 <mahmoud_abdalla@live.com>
2024-06-26 06:56:10 +0200

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+import os, sys
+number1 = int(10) 
+number2 = int(5)
+sum = number1 + number2
+subtraction = number1 - number2
+product = number1 * number2
+print("Addition of", (number1), "and", (number2), "is", (sum))
+print("Subtraction of", (number1), "and", (number2), "is", (subtraction))
+print("Multiplications of", (number1), "and", (number2), "is", (product))
