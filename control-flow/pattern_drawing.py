4d5b5e998b163af85e8662f2b9788a728e19aa72
mallah9 <mahmoud_abdalla@live.com>
2024-07-06 12:56:53 +0200

    while loop

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+while True:
+    size = input("Enter the size of the patter (positive integer): ")
+    if not size.isdigit() or int(size) <= 0:
+        print("Please enter a positive integer.")
+        continue
+    size = int(size)
+    for i in range(size):
+        for j in range(size):
+            print("*", end="")
+        print()
+    break    
