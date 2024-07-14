744e9ed529880a7d280ecf56eb3ca52a6d34760f
mallah9 <mahmoud_abdalla@live.com>
2024-07-14 03:28:42 +0300

    shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -16,7 +16,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item name: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} added to the list!")
         elif choice == '2':
