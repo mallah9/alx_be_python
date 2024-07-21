5584ac67a7e07581e1d306a85adaccaa4a5dba8d
mallah9 <mahmoud_abdalla@live.com>
2024-07-21 10:55:24 +0300

    library system  inheritance and composition)

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,15 +3,24 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)  # Call base class constructor
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {super().__str__()}, File Size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)  # Call base class constructor
         self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {super().__str__()}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
